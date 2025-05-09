a7c6e5879948762eed7596b6f67665769c9a2a1d
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-05-09 09:22:55 +0530

    added notes for visibility modifiers

---
+++ src/classy/accessModifiers.kt
@@ -0,0 +1,10 @@
+// defines where a function, class or variable is visible in a project
+// kotlin gives 4 modifiers : public, private, internal, protected
+// a public class/func./var is accessible everywhere
+// a private func is accessible only in same class where it's defined, a private var is accessible in same file
+// protected = same as private, but also accessible in subclasses
+// internal = access in same module
+
+// If you override a protected or an internal member and do not specify the visibility explicitly...
+// ...the overriding member will also have the same visibility as the original
+
